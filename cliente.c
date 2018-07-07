75c4cd9e3d02a8d119624eac90405584a50433fc
Fede Alessio <alessiofederico@thomsonreuters.com>
2018-07-07 13:30:37 -0300

    Iniciando proyecto tp final

---
+++ cliente.c
@@ -0,0 +1,253 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+#define ipServidor "192.168.1.105"
+#define puertoServidor 6666
+#define BUFFER 1024
+
+void EnviarQuery(int idsockc);
+void EnviarRecibirMensaje(int idsockc, char *msj);
+void EditarArchivo();
+void EnviarArchivoServidor(int idsockc);
+char* SeleccionarBase();
+
+int main (int argc, char *argv[])
+{
+	// Conectar con el servidor de archivos
+	int idsockc,idso;
+	struct sockaddr_in c_sock;
+	int lensock;
+
+	idsockc = socket(AF_INET, SOCK_STREAM, 0);
+
+	printf("Conexion con el servidor: %d.\n", idsockc);
+	c_sock.sin_family = AF_INET;
+	c_sock.sin_addr.s_addr = inet_addr(ipServidor);
+	c_sock.sin_port = htons(puertoServidor);
+
+	lensock = sizeof(struct sockaddr_in);
+	lensock = sizeof(c_sock);
+
+	idso = connect(idsockc,(struct sockaddr *)&c_sock,lensock);
+
+	if(idso!=-1)
+	{
+		int op;
+		do{
+			system("clear");
+			printf("************** \n");
+			printf("**** MENU **** \n");
+			printf("************** \n");
+			printf("1. Ejecutar Query \n");
+			printf("2. Mostrar Catálogo \n");
+			printf("3. Ingresar Query \n");
+			printf("0. Salir \n");
+			printf("\n\nOpcion: ");
+
+			scanf("%d",&op);
+
+			fflush(stdin);
+
+			switch(op)
+			{
+				case 1:
+				{
+					EnviarQuery(idsockc);
+				} break;
+				case 2:
+				{
+		//			EditarArchivo();
+				} break;
+				case 3:
+				{
+			//		EnviarArchivoServidor(idsockc);s
+				} break;
+			}
+		} while(op != 0);
+	}
+	else
+	{
+		printf("Conexion rechazada! %d\n",idso);
+
+	}
+
+	printf("\nDesconectando del servidor: %d.\n\n", idsockc);
+
+	write(idsockc, "salir", 5);
+
+	close(idsockc);
+}
+
+void EnviarRecibirMensaje(int idsockc, char *msj)
+{
+	int nb;
+	char buf[BUFFER];
+	// Envia mensaje
+	nb=strlen(msj);
+	printf("Mensaje a enviar al servidor %d: %s\n",idsockc,msj);
+	write(idsockc,msj,nb);
+
+	sleep(1);
+
+	// Recibe respuesta
+	nb=read(idsockc,buf,BUFFER);
+	buf[nb]='\0';
+	printf("Recibido del servidor %d: %s\n", idsockc, buf);
+}
+
+char* SeleccionarBase()
+{
+	char * op = (char *) malloc(2);
+	memset(op, 0, 2);
+	system("clear");
+	printf("************** \n");
+	printf("1. MySQL \n");
+	printf("2. PostgreSQL\n");
+	printf("0. Salir \n");
+	printf("\n\nOpcion: ");
+
+	scanf("%s",&op);
+	fflush(stdin);
+	
+	return op;
+}	
+
+void EnviarQuery(int idsockc)
+{
+	//char* opDB = (char *) malloc(2);
+	//memset(opDB, 0, 2);
+	
+	//strcpy(opDB, SeleccionarBase());
+	//char * op = (char *) malloc(1);
+	//memset(op, 0, 1);
+	int op;
+	system("clear");
+	printf("************** \n");
+	printf("1. MySQL \n");
+	printf("2. PostgreSQL\n");
+	printf("0. Salir \n");
+	printf("\n\nOpcion: ");
+
+	scanf("%d",&op);
+	fflush(stdin);
+	
+	printf("\nBase de datos selccionada %d: ", op);
+	
+	// Escribimos comando para enviar un archivo
+	
+	if(op == 1){
+		write(idsockc, "1", 1);
+	} 
+	else {
+		if(op == 2)
+			write(idsockc, "2", 1);
+		else {
+			printf("\nPresione una tecla para volver.");
+			getchar();
+			return;
+		}
+	}
+	// sleep(2);
+
+	char * buf = (char *)malloc(BUFFER);
+	int nb;
+	char * nombreArchivo = (char *)malloc(50);
+	memset(nombreArchivo, 0, 50);
+	FILE *archivo;
+
+	//char * query = (char *)malloc(BUFFER);
+	//memset(query, 0, BUFFER);
+
+	char query[2048];
+	char enter;
+	
+	/*printf("Lista de archivos disponibles: \n");
+	system("ls");
+	printf("Ingrese el nombre del archivo que quiere enviar: \n");
+
+	scanf("%s", &nombreArchivo[0]);
+
+	if((archivo = fopen(nombreArchivo, "rb")) == NULL)
+	{
+		printf("\nNo se puede abrir el archivo!");
+		exit(0);
+	}*/
+	
+ //   strcpy(query, "select * from empleado");
+	printf("\nIngrese la query: \n");
+	//fflush(stdin);
+	scanf("%c", &enter);	
+	gets(query);
+	printf("\nQuery: %s", query);
+	write(idsockc, query, BUFFER);
+
+	sleep(1);
+
+	/*int seguirEnviando = 0;
+	buf[0]='\0';
+	printf("\nBuffer1... %s \n", buf);
+	// Leemos la data y la enviamos
+	while(seguirEnviando == 0)
+	{
+		// Particionamos el archivo en 256 bytes
+		// unsigned char buff[1024] = {0};
+		int nread = fread(buf, 1, BUFFER, archivo);
+		printf("\nBuffer2... %s \n", buf);
+		printf("\nBytes leidos %d \n", nread);
+
+		if(nread > 0)
+		{
+			printf("Enviando... \n");
+			write(idsockc, buf, nread);
+		}
+		if (nread < BUFFER)
+		{
+			if (feof(archivo))
+			{
+				printf("End of file\n");
+				printf("File transfer completed for id: %d\n", idsockc);
+				sleep(1);
+				write(idsockc, "fin", 3);
+			}
+			if (ferror(archivo))
+				printf("Error reading\n");
+
+			printf("1\n");
+			seguirEnviando++;
+			fclose(archivo);
+			printf("2\n");
+		}
+	}*/
+	
+	// Recibe el nombre del archivo
+	nb = read(idsockc, buf, BUFFER);
+	buf[nb] = '\0';
+	printf("\nResultado de la query \n %s", buf);
+
+	printf("\nPresione una tecla para volver.");
+	getchar();
+	getchar();
+
+}
+
+void EditarArchivo() {
+	char * nombreArchivo = (char *) malloc(50);
+	memset(nombreArchivo, 0, 50);
+
+	printf("Lista de archivos disponibles: \n");
+	system("ls");
+	printf("Ingrese el nombre del archivo que quiere enviar: \n");
+
+	scanf("%s", &nombreArchivo[0]);
+
+	// Abrimos la edicion
+	system("gedit archivo1.txt");
+}
---
+++ servidor.c
@@ -0,0 +1,223 @@
+#include <libpq-fe.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <mysql.h>
+
+#define IPSerBD "192.168.1.105"
+#define PuertoSerBD 6666
+#define BUFFER 1024
+
+#define IPPostgrsql "127.0.0.1"
+#define PuertoPostgresql "5432"
+
+void funcionPostgresql(int idsockc);
+void funcionMysql(int idsockc);
+void funcionHilo(void);
+void EjecutarQueryYEnviarResultado(int idsockc);
+
+struct sockaddr_in c_sock;
+int idsockc=0;
+
+int main(int argc, char *argv[])
+{	
+	struct sockaddr_in s_sock;
+	int idsocks;
+	int lensock = sizeof(struct sockaddr_in);
+	idsocks = socket(AF_INET, SOCK_STREAM, 0);
+	printf("idsocks %d\n", idsocks);
+
+	s_sock.sin_family      = AF_INET;
+	s_sock.sin_port        = htons(PuertoSerBD);
+	s_sock.sin_addr.s_addr = inet_addr(IPSerBD);
+	memset(s_sock.sin_zero,0,8);
+	printf("bind %d\n", bind(idsocks,(struct sockaddr *) &s_sock,lensock));
+	printf("listen %d\n", listen(idsocks,5));
+	
+	while(1)
+	{
+		printf("Servidor esperando conexion...\n");
+		idsockc = accept(idsocks,(struct sockaddr *)&c_sock,&lensock);
+		if(idsockc != -1)
+		{			
+			pthread_t hilo;
+			pthread_create(&hilo, NULL, (void*)funcionHilo, NULL);
+		}
+		else
+		{
+			printf("Conexion rechazada %d!\n", idsockc);
+		}
+	}
+	
+	return 0 ;
+}
+
+void funcionHilo(void)
+{
+	// UbicacionDelCliente(c_sock);
+	EjecutarQueryYEnviarResultado(idsockc);
+	pthread_exit(0);
+}
+
+void funcionMysql(int idsockc)
+{
+	MYSQL *conn;
+	MYSQL_RES *res;
+	MYSQL_ROW row;
+	char *server = "localhost";
+	char *user = "root";
+	char *password = "toor";  
+	char *database = "autosdb";
+	int nb;
+	char * query = (char *)malloc(BUFFER);
+	memset(query, 0, BUFFER);
+	
+	conn = mysql_init(NULL);
+	
+	
+	// Recibe el nombre del archivo
+	nb = read(idsockc, query, BUFFER);
+	// query[nb] = '\0';
+	printf("\nRecibido del cliente %d: query: %s \n", idsockc, query);
+
+   /* Connect to database */
+   if (!mysql_real_connect(conn, server,
+         user, password, database, 0, NULL, 0)) {
+      fprintf(stderr, "%s\n", mysql_error(conn));
+      exit(1);
+   }
+
+   /* send SQL query */
+//   if (mysql_query(conn, "show tables")) {
+//      fprintf(stderr, "%s\n", mysql_error(conn));
+//      exit(1);
+//   }
+
+//   res = mysql_use_result(conn);
+//   /* output table name */
+//   printf("MySQL Tables in mysql database:\n");
+//   while ((row = mysql_fetch_row(res)) != NULL)
+//      printf("%s \n", row[0]);
+
+   /* send SQL query */
+   if (mysql_query(conn, query)) { // "SELECT * from autos"
+      fprintf(stderr, "%s\n", mysql_error(conn));
+      exit(1);
+   }
+
+   res = mysql_use_result(conn);
+   /* output table name */
+   printf("Autos in mysql database:\n");
+
+	unsigned int num_fields;
+	unsigned int i;
+
+	num_fields = mysql_num_fields(res);
+	while ((row = mysql_fetch_row(res)))
+	{
+	   unsigned long *lengths;
+	   lengths = mysql_fetch_lengths(res);
+	   for(i = 0; i < num_fields; i++)
+	   {
+		   printf("[%.*s] ", (int) lengths[i],
+				  row[i] ? row[i] : "NULL");
+	   }
+	   printf("\n");
+	}
+  
+   /* close connection */
+   mysql_free_result(res);
+   mysql_close(conn);
+   write(idsockc, "Respuesta", 1024);
+}
+
+void funcionPostgresql(int idsockc)
+{
+	PGconn *conn;
+	PGresult *res;
+	int i,j;
+	char respuesta[BUFFER];
+	memset(respuesta, 0, BUFFER);
+	
+	int nb;
+	char * query = (char *)malloc(BUFFER);
+	memset(query, 0, BUFFER);
+	// Recibe el nombre del archivo
+	nb = read(idsockc, query, BUFFER);
+	// query[nb] = '\0';
+	printf("\nRecibido del cliente %d: query: %s \n", idsockc, query);
+	
+	conn =  PQsetdbLogin(IPPostgrsql, PuertoPostgresql, NULL, NULL, "sodsql", "postgres", "toor");
+	if (PQstatus(conn) != CONNECTION_BAD)
+	{
+		res = PQexec(conn, query); //"SELECT * FROM employees"
+		if (res != NULL && PGRES_TUPLES_OK == PQresultStatus(res))
+		{
+			for (i = 0 ; i <= PQntuples(res)-1;  i++)
+			{
+				for (j = 0 ; j < PQnfields(res); j++)
+				{
+					strcat(respuesta,PQgetvalue(res,i,j));
+					strcat(respuesta,"\t");                     
+					printf("%s\t", PQgetvalue(res,i,j));
+				}
+				strcat(respuesta,"\n");
+				printf("\n");
+			}
+			strcat(respuesta,"\0");
+			PQclear(res);
+		} else {
+			printf("Fallo query");
+		}
+	} else {
+		printf("Fallo conexion");
+	}
+	PQfinish(conn);
+	write(idsockc, respuesta, 1024);
+}
+
+void EjecutarQueryYEnviarResultado(int idsockc)
+{
+	char * buf = (char *)malloc(BUFFER);
+	int nb; 
+	char * tipoDB = (char *)malloc(10);
+	memset(tipoDB, 0, 10);
+	
+	printf("Conexion aceptada desde el cliente %d.\n", idsockc);
+	
+	// Leemos la opcion de la base (1=mysql, 2=postgres, 0=salir)
+	nb = read(idsockc, tipoDB, 10);
+	
+	tipoDB[nb] = '\0';
+	printf("\nRecibido del cliente %d: %s \n", idsockc, tipoDB);
+
+	while(strncmp(tipoDB, "0", 1) != 0)
+	{
+		if (strncmp(tipoDB, "1", 1) == 0) // mysql
+		{
+			printf("\nEjecutamos funcion MYSQL %s \n", tipoDB);
+			fflush(stdin);
+			funcionMysql(idsockc);
+		}
+		if (strncmp(tipoDB, "2", 1) == 0) // postgres
+		{
+			printf("\nEjecutamos funcion POSTGRES %s \n", tipoDB);
+			fflush(stdin);
+			funcionPostgresql(idsockc);
+		}		
+	
+		printf("\nIterando ciclo de comandos\n");
+		sleep(1);
+		nb = read(idsockc, tipoDB, BUFFER);
+		buf[nb]='\0';
+		printf("\nRecibido del cliente %d: %s\n", idsockc, tipoDB);
+	 }
+	printf("\nCerrando conexión del cliente %d.\n", idsockc);
+	close(idsockc); 
+}
